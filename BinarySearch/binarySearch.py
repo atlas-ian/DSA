e03c7ea8bc86ea068b6beba98400031ef1d3134c
adarshaadi06 <adarshn54688@gmail.com>
2025-08-21 11:13:12 +0530

    basics of python

---
+++ BinarySearch/binarySearch.py
@@ -0,0 +1,20 @@
+def BinarySearch(nums, target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+ 
+
+        if target == nums[mid]:
+            return nums[mid]
+        elif target < nums[mid]:
+            right = nums[mid] - 1
+        else:
+            left = nums[mid] + 1
+
+    return "not found"
+
+arr = [1,3,5,7,9,11,14]
+
+print(BinarySearch(arr, 14))

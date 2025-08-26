2370a547b5da1d7d6b19e19833d9750efcaf3f01
atlas-ian <adarshn54688@gmail.com>
2025-08-26 11:50:26 +0530

    problem 2- python problem solving in hackerrank

---
+++ Hackerrank/python-print.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+if __name__ == "__main__":
+    n = int(input())
+    # sum = 0
+    for i in range(1, n + 1):
+        print(i, end = "")
+        # sum += i
+        
+    # print(i, end = "")

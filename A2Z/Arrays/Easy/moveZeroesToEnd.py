7b6cfc576fb35908648981af688350b09bf54604
atlas-ian <adarshn54688@gmail.com>
2025-09-01 13:22:38 +0530

    move zeroes to the end - brute force - O(2N), O(N)

--- A2Z/Arrays/Easy/moveZeroesToEnd.py
+++ A2Z/Arrays/Easy/moveZeroesToEnd.py
@@ -4,7 +4,15 @@ def moveZeroesToEnd(nums):
     for i in range(0, len(nums)):
         if nums[i] != 0:
             temp.append(nums[i])
-    return temp
+    # return temp
+
+    for i in range(0, len(temp)):
+        nums[i] = temp[i]
+    # return nums
+
+    for i in range(len(temp), len(nums)):
+        nums[i] = 0
+    return nums 
 
 if __name__ == "__main__":
     arr = [1,0,2,0,3,4,0,0]

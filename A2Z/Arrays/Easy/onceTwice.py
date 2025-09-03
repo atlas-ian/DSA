4b801840476f4912e2a434263d61a824f9d0c05f
atlas-ian <adarshn54688@gmail.com>
2025-09-03 14:11:45 +0530

    Number that appear once reamaining all appears twice - Optimal using XOR

--- A2Z/Arrays/Easy/onceTwice.py
+++ A2Z/Arrays/Easy/onceTwice.py
@@ -1,4 +1,4 @@
-# number that appear once reamaining all appears twice - Brute force
+# Number that appear once reamaining all appears twice - Brute force
 
 def onceTwice(nums):
     for i in nums:
@@ -10,7 +10,14 @@ def onceTwice(nums):
                 count += 1
         if count == 1:
             return num
+        
+
+def onceTwiceOptimal(nums):
+    xor = 0
+    for i in nums:
+        xor = xor ^ i
+    return xor
     
 if __name__ == "__main__":
-    arr = [1,2,4,5,1,2,4,5,8,8]
-    print(onceTwice(arr))
+    arr = [1,2,4,5,1,4,5,8,8]
+    print(onceTwiceOptimal(arr))

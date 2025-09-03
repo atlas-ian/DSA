2588ac270a4f32d557cd1cccbbf60666e9dbd611
atlas-ian <adarshn54688@gmail.com>
2025-09-03 14:09:41 +0530

    Number that appear once reamaining all appears twice - Better approach

--- A2Z/Arrays/Easy/max_element.py
+++ A2Z/Arrays/Easy/max_element.py
@@ -5,8 +5,19 @@ def max_element(nums):
     for i in nums:
         maxi = max(maxi, i)
 
+    # return maxi
+
+    hash_map = { i:0 for i in range(maxi + 1)}
+    for i in nums:
+        hash_map[i] += 1
+
+
+    for i in nums:
+        if hash_map[i] == 1:
+            return i
+        
     return maxi
 
 if __name__ == "__main__":
-    arr = [1,4,5,1,2,4,5]
+    arr = [1,4,5,1,2,2,4,5,6]
     print(max_element(arr))
